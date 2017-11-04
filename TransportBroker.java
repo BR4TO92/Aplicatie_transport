ad95d660a4a590b6784ba093ce397b149dcbb3a4
BR4TO92 <33318117+BR4TO92@users.noreply.github.com>
2017-11-04 12:59:20 +0200

    3.0

--- TransportBroker.java
+++ TransportBroker.java
@@ -3,20 +3,20 @@
  * @author Daniel Bratosin
  *
  */
-public class TransportBroker implements ITransport
+public class TransportBroker
 {
 	@SuppressWarnings("unused")
-	private User user;
+	private UserRequest userRequest;
 	private int lowestPrice = 0;
 	private String transportVehicle = "";
 	
 	/**
 	 * This method will pass a newly created user in the transport broker, from the main method, for later computations.
-	 * @param user A single user object
+	 * @param userRequest A single user object
 	 */
-	public void setUser(User user)
+	public void setUser(UserRequest userRequest)
 	{
-		this.user = user;
+		this.userRequest = userRequest;
 	}
 	
 	public int getLowestPrice()
@@ -28,69 +28,48 @@ public class TransportBroker implements ITransport
 	{
     	return this.transportVehicle;
 	}
-	
-    /**
-     * This method computes the lowest possible price, based on the priority level from a single user.
-     * @param user An user object used for getting the priority level and quantity from it, for the computation of the lowest price possible
-     */
-    public void computeLowestPrice(User user)
-	{
-		byte priorityLevel = user.getPriorityLevel();
-		int quantity = user.getQuantity();
-		
-		switch(priorityLevel)
-		{
-			case 1: computeCostForPriorityLevel(quantity, PriorityLevel.VERY_URGENT.getPriorityLevel());
-			        break;
-			                    
-			case 2: computeCostForPriorityLevel(quantity, PriorityLevel.URGENT.getPriorityLevel());
-                    break;
-                         
-			case 3: computeCostForPriorityLevel(quantity, PriorityLevel.NORMAL.getPriorityLevel());
-                    break;
-                         
-			case 4: computeCostForPriorityLevel(quantity, PriorityLevel.LOW.getPriorityLevel());
-                    break;
-		}
-	}
 
 	/**
-	 * This method computes the lowest possible price based on the priority level and the quantity from a single user.
+	 * This method computes the lowest possible price or the price based on the fastest vehicle possible, based on the priority
+	 * level and the quantity from a single user.
 	 * @param quantity The quantity from an user object used for computation of the quantity cost
 	 * @param priorityLevel The priority level from an user object used for computation of the priority level cost
+	 * @param isSpeedEfficient true/false if the user chose the fastest vehicle
 	 */
-	private void computeCostForPriorityLevel(int quantity, int priorityLevel)
-	{
+	public void computeLowestPrice(UserRequest userRequest, boolean isSpeedEfficient)
+	{		
+		byte priorityLevel = userRequest.getPriorityLevel();
+		int quantity = userRequest.getQuantity();
 		int costForPriorityLevel;
 		
-		if(priorityLevel == PriorityLevel.VERY_URGENT.getPriorityLevel())
+		if(priorityLevel == EnumPriorityLevel.VERY_URGENT.getPriorityLevel())
 		{
-			costForPriorityLevel = RateForEmergencyLevel.VERY_URGENT_RATE.getRateForEmergencyLevel();
+			costForPriorityLevel = EnumRateForEmergencyLevel.VERY_URGENT_RATE.getRateForEmergencyLevel();
 		}
 		
-		else if(priorityLevel == PriorityLevel.URGENT.getPriorityLevel())
+		else if(priorityLevel == EnumPriorityLevel.URGENT.getPriorityLevel())
 		{
-			costForPriorityLevel = RateForEmergencyLevel.URGENT_RATE.getRateForEmergencyLevel();
+			costForPriorityLevel = EnumRateForEmergencyLevel.URGENT_RATE.getRateForEmergencyLevel();
 		}
 		
-		else if(priorityLevel == PriorityLevel.NORMAL.getPriorityLevel())
+		else if(priorityLevel == EnumPriorityLevel.NORMAL.getPriorityLevel())
 		{
-			costForPriorityLevel = RateForEmergencyLevel.NORMAL_RATE.getRateForEmergencyLevel();
+			costForPriorityLevel = EnumRateForEmergencyLevel.NORMAL_RATE.getRateForEmergencyLevel();
 		}
 		
 		else
 		{
-			costForPriorityLevel = RateForEmergencyLevel.LOW_RATE.getRateForEmergencyLevel();
+			costForPriorityLevel = EnumRateForEmergencyLevel.LOW_RATE.getRateForEmergencyLevel();
 		}
 		
 		if(quantity == 1)
 		{
-			computeCostForLowQuantity(quantity, costForPriorityLevel);
+			computeCostForLowQuantity(quantity, costForPriorityLevel, isSpeedEfficient);
 		}
 					
 		if((quantity >= 2) && (quantity <= 50))
 		{
-			computeCostFormediumQuantity(quantity, costForPriorityLevel);
+			computeCostForMediumQuantity(quantity, costForPriorityLevel, isSpeedEfficient);
 		}
 						
 		if(quantity > 50)
@@ -100,9 +79,8 @@ public class TransportBroker implements ITransport
 	}
 
 	/**
-	 * This method computes the price for each transport vehicle based on the priority level and the pallet rate for each vehicle. It only
-	 * computes the price for a high quantity of pallets. After the price for each one is computed, only one will be selected as lowest
-	 * possible, based on finding the minimal one. Also finds the right vehicle.
+	 * This method computes the price and the vehicle used for the transport based on high priority level and the pallet rate for the vehicle
+	 * used. It only computes the price for a high quantity of pallets. In this case, for a single vehicle, a single price will be computed.
 	 * @param quantity The quantity from an user object used for computation of the quantity cost
 	 * @param costForPriorityLevel The priority level cost
 	 */
@@ -110,77 +88,69 @@ public class TransportBroker implements ITransport
 	{
 		int costForShip;
 		
-		costForShip = PalletRateForVehicle.PALLET_RATE_FOR_SHIP.getPalletRateForVehicle() * quantity + costForPriorityLevel;
+		costForShip = EnumPalletRateForVehicle.PALLET_RATE_FOR_SHIP.getPalletRateForVehicle() * quantity + costForPriorityLevel;
+		
 		this.lowestPrice = costForShip;
-		this.transportVehicle = TransportVehicle.SHIP.getTransportVehicle();
+		this.transportVehicle = EnumTransportVehicle.SHIP.getTransportVehicle();
 	}
 
 	/**
-	 * This method computes the price for each transport vehicle based on the priority level and the pallet rate for each vehicle. It only
-	 * computes the price for a medium quantity of pallets. After the price for each one is computed, only one will be selected as lowest
-	 * possible, based on finding the minimal one. Also finds the right vehicle.
+	 * This method computes the price and the vehicle used for each transport vehicle based on the priority level and the pallet rate for
+	 * each vehicle. It only computes the price for a medium quantity of pallets. After the price for each vehicle is computed, one will be
+	 * selected as the lowest price possible (the minimal one), or the one for the fastest vehicle.
 	 * @param quantity The quantity from an user object used for computation of the quantity cost
 	 * @param costForPriorityLevel The priority level cost
+	 * @param isSpeedEfficient true/false if the user chose the fastest vehicle
 	 */
-	private void computeCostFormediumQuantity(int quantity, int costForPriorityLevel)
+	private void computeCostForMediumQuantity(int quantity, int costForPriorityLevel, boolean isSpeedEfficient)
 	{
 		int costForTrain;
 		int costForTruck;
 		
-		costForTrain = PalletRateForVehicle.PALLET_RATE_FOR_TRAIN.getPalletRateForVehicle() * quantity + costForPriorityLevel;
-		costForTruck = PalletRateForVehicle.PALLET_RATE_FOR_TRUCK.getPalletRateForVehicle() * quantity + costForPriorityLevel;
-		this.lowestPrice = Math.min(costForTrain, costForTruck);
+		costForTrain = EnumPalletRateForVehicle.PALLET_RATE_FOR_TRAIN.getPalletRateForVehicle() * quantity + costForPriorityLevel;
+		costForTruck = EnumPalletRateForVehicle.PALLET_RATE_FOR_TRUCK.getPalletRateForVehicle() * quantity + costForPriorityLevel;
 		
-		if(this.lowestPrice == costForTrain)
+		if(isSpeedEfficient)
 		{
-			this.transportVehicle = TransportVehicle.TRAIN.getTransportVehicle();
+			this.lowestPrice = costForTruck;
+			this.transportVehicle = EnumTransportVehicle.TRUCK.getTransportVehicle();
 		}
 		
 		else
 		{
-			this.transportVehicle = TransportVehicle.TRUCK.getTransportVehicle();
+			this.lowestPrice = costForTrain;
+			this.transportVehicle = EnumTransportVehicle.TRAIN.getTransportVehicle();
 		}
 	}
 
 	/**
-	 * This method computes the price for each transport vehicle based on the priority level and the pallet rate for each vehicle. It only
-	 * computes the price for a low quantity of pallets. After the price for each one is computed, only one will be selected as lowest
-	 * possible, based on finding the minimal one. Also finds the right vehicle.
+	 * This method computes the price and the vehicle used for each transport vehicle based on the priority level and the pallet rate for
+	 * each vehicle. It only computes the price for a low quantity of pallets. After the price for each vehicle is computed, one will be
+	 * selected as the lowest price possible (the minimal one), or the one for the fastest vehicle.
 	 * @param quantity The quantity from an user object used for computation of the quantity cost
 	 * @param costForPriorityLevel The priority level cost
+	 * @param isSpeedEfficient true/false if the user chose the fastest vehicle
 	 */
-	private void computeCostForLowQuantity(int quantity, int costForPriorityLevel)
+	private void computeCostForLowQuantity(int quantity, int costForPriorityLevel, boolean isSpeedEfficient)
 	{
 		int costForCar;
 		int costForPlane;
 		int costForHelicopter;
 		
-		costForCar = PalletRateForVehicle.PALLET_RATE_FOR_CAR.getPalletRateForVehicle() * quantity + costForPriorityLevel;
-		costForPlane = PalletRateForVehicle.PALLET_RATE_FOR_PLANE.getPalletRateForVehicle() * quantity + costForPriorityLevel;
-		costForHelicopter = PalletRateForVehicle.PALLET_RATE_FOR_HELICOPTER.getPalletRateForVehicle() * quantity + costForPriorityLevel;
-		this.lowestPrice = minimumOfThreeNumbers(costForCar, costForPlane, costForHelicopter);
+		costForCar = EnumPalletRateForVehicle.PALLET_RATE_FOR_CAR.getPalletRateForVehicle() * quantity + costForPriorityLevel;
+		costForPlane = EnumPalletRateForVehicle.PALLET_RATE_FOR_PLANE.getPalletRateForVehicle() * quantity + costForPriorityLevel;
+		costForHelicopter = EnumPalletRateForVehicle.PALLET_RATE_FOR_HELICOPTER.getPalletRateForVehicle() * quantity + costForPriorityLevel;
 		
-		if(this.lowestPrice == costForCar)
+		if(isSpeedEfficient)
 		{
-			this.transportVehicle = TransportVehicle.CAR.getTransportVehicle();
-		}
-		
-		else if(this.lowestPrice == costForPlane)
-		{
-			this.transportVehicle = TransportVehicle.PLANE.getTransportVehicle();
+			this.lowestPrice = costForHelicopter;
+			this.transportVehicle = EnumTransportVehicle.HELICOPTER.getTransportVehicle();
 		}
 		
 		else
 		{
-			this.transportVehicle = TransportVehicle.HELICOPTER.getTransportVehicle();
+			this.lowestPrice = costForCar;
+			this.transportVehicle = EnumTransportVehicle.CAR.getTransportVehicle();
 		}
-	}
-	
-	/**
-	 * @return the minimum of three numbers given as parameters
-	 */
-	private int minimumOfThreeNumbers(int a, int b, int c)
-	{
-	    return Math.min(Math.min(a, b), c);
 	}
 }
